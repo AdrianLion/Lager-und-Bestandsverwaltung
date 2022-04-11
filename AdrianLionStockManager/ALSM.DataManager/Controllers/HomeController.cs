cbc27a911a5e874784d687d40562d02a3bc23962
Adrian Lion <adrian.lion@online.de>
2022-04-11 12:09:15 +0200

    Cahnged .NET Version switched back from 6 to 5 since some dependencies I'm going to use at later stages are not supported yet.

--- AdrianLionStockManager/ALSM.DataManager/Controllers/HomeController.cs
+++ AdrianLionStockManager/ALSM.DataManager/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using ALSM.DataManager.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ALSM.DataManager.Controllers
 {

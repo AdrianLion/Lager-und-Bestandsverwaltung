3f3687e533c0057f62c56fb3a157447c327cb3da
Adrian Lion <adrian.lion@online.de>
2022-04-11 11:59:28 +0200

    Create WebAPI Project Adds a default WebAPI Project

---
+++ AdrianLionStockManager/ALSM.DataManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ALSM.DataManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
